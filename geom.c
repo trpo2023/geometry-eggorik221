e6eeeddbe9d4d9f372fed0c4035f1283a7addcb2
eggorik <eggorik221@users.noreply.github.com>
2023-02-27 18:40:37 +0700

    Add input validation

--- geom.c
+++ geom.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define Size 30
 
 typedef struct point
 {
@@ -14,7 +15,28 @@ typedef struct circle
     double r;
 } Circle;
 
+
+
 int main(){
-    
+    char str[Size];
+    char curch =getchar();
+    int symbnum = 0;
+    Circle circle;
+    do
+    {
+        if (curch == '('){
+            if (strcmp(str, "circle") == 0){
+                //Get info about circle
+            }
+            else
+            {
+                puts("Error at column 0: expected 'circle'\n");
+                exit(0);
+            }
+        }
+        str[symbnum++] = curch;
+
+    } while ((curch = getchar()) != '\n' || curch != EOF);
+
     return 0;
 }
